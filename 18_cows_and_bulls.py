436ad83255e859be9372fec8a0efc19c13cca2d8
gokhankar <63194790+gokhankar@users.noreply.github.com>
2020-08-08 01:02:18 +0300

    Add files via upload

--- 18_cows_and_bulls.py
+++ 18_cows_and_bulls.py
@@ -3,34 +3,47 @@
 import random
 
 
-def generate_number():
-    return random.randint(1000, 10000)
-
-
 def make_list():
-    return list(str(random_number))
+    generated_list = [str(random.randint(1, 9))]
+    while len(generated_list) < 4:
+        elem = str(random.randint(0, 9))
+        if elem not in generated_list:
+            generated_list.append(elem)
+    return generated_list
 
 
 def get_num(help_text="Guess a 4-digit number: "):
-    return input(help_text)
+    while True:
+        try:
+            num = int(input(help_text))
+        except:
+            print("Please type a 4-digit number")
+            continue
+        if 10000 > num > 999:
+            return num
+        else:
+            print("Please type a 4-digit number")
 
 
 def cows_and_bulls():
-    cows = 0
-    bulls = 0
+    global count
+    cows, bulls = 0, 0
     for i in range(0, 4):
         if guess_list[i] == my_list[i]:
             cows += 1
         elif guess_list[i] in my_list:
             bulls += 1
     print(f"cows={cows} and bulls={bulls}")
+    count += 1
+    if cows == 4:
+        print(f"Congratulations!!!\nYou succeeded in {count}. try")
+        quit()
 
 
-random_number = generate_number()
 my_list = make_list()
 print(my_list)
 print("Lets play Cows and bulls!\nI generated a 4-digit number. ")
+count = 0
 while True:
     guess_list = list(str((get_num())))
-    print(guess_list)
     cows_and_bulls()
