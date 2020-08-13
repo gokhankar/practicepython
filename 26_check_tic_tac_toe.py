003173547c7fa996d4f8a48d62602e4ceb9078c6
gokhankar <63194790+gokhankar@users.noreply.github.com>
2020-08-13 13:01:15 +0300

    Add files via upload

---
+++ 26_check_tic_tac_toe.py
@@ -0,0 +1,57 @@
+# we are trying to build up to a full tic-tac-toe board. However, this is significantly more than half an hour of
+# coding, so we’re doing it in pieces. Today, we will simply focus on checking whether
+# someone has WON a game of Tic Tac Toe, not worrying about how the moves were made.
+
+# import random
+
+"""
+def random_game():
+    game = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    for item in game:
+        for elem in item:
+            elem = 1
+            print(game)
+"""
+
+
+def find_winner():
+    if game[0][0] == 1 and game[0][1] == 1 and game[0][2] == 1:
+        return 1
+    elif game[0][0] == 2 and game[0][1] == 2 and game[0][2] == 2:
+        return 2
+    elif game[1][0] == 1 and game[1][1] == 1 and game[1][2] == 1:
+        return 1
+    elif game[1][0] == 2 and game[1][1] == 2 and game[1][2] == 2:
+        return 2
+    elif game[2][0] == 1 and game[2][1] == 1 and game[2][2] == 1:
+        return 1
+    elif game[2][0] == 2 and game[2][1] == 2 and game[2][2] == 2:
+        return 2
+    elif game[0][0] == 1 and game[1][0] == 1 and game[2][0] == 1:
+        return 1
+    elif game[0][0] == 2 and game[1][0] == 2 and game[2][0] == 2:
+        return 2
+    elif game[0][1] == 1 and game[1][1] == 1 and game[2][1] == 1:
+        return 1
+    elif game[0][1] == 2 and game[1][1] == 2 and game[2][1] == 2:
+        return 2
+    elif game[0][2] == 1 and game[1][2] == 1 and game[2][2] == 1:
+        return 1
+    elif game[0][2] == 2 and game[1][2] == 2 and game[2][2] == 2:
+        return 2
+    elif game[0][0] == 1 and game[1][1] == 1 and game[2][2] == 1:
+        return 1
+    elif game[0][0] == 2 and game[1][1] == 2 and game[2][2] == 2:
+        return 2
+    elif game[0][2] == 1 and game[1][1] == 1 and game[2][0] == 1:
+        return 1
+    elif game[0][2] == 2 and game[1][1] == 2 and game[2][0] == 2:
+        return 2
+    else:
+        return "draw"
+
+
+game = [[1, 2, 0], [2, 1, 0], [2, 1, 0]]
+
+
+print(find_winner())
