1d113652317b781d320216aab023a4cc12667c03
gokhankar <63194790+gokhankar@users.noreply.github.com>
2020-08-14 23:04:47 +0300

    Add files via upload

--- 36_birthday_plots.py
+++ 36_birthday_plots.py
@@ -1,8 +1,8 @@
 # In this exercise, use the bokeh Python library to plot a histogram of which months the scientists have birthdays in!
 
-
 import json
 from collections import Counter
+from bokeh.plotting import output_file, figure, show
 
 with open("info.json", "r") as f:
     birthdays = json.load(f)
@@ -28,3 +28,12 @@ for name, birthday_string in birthdays.items():
     months.append(num_to_string[month])
 
 print(Counter(months))
+
+output_file("plot.html")
+x_category = ['January', 'February', 'March', 'April', 'May', 'June',
+              'July', 'August', 'September', 'October', 'November', 'December']
+x = list(Counter(months).keys())
+y = list(Counter(months).values())
+fig = figure(x_range=x_category)
+fig.vbar(x=x, top=y, width=0.5)
+show(fig)
